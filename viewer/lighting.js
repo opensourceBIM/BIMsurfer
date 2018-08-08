5dc9eb8d023ae629f9c3a9b8f70664188e649e0d
xeolabs <lindsay.kay@xeolabs.com>
2018-08-08 14:14:58 +0200

    Allow light source to be dynamic (helps with debugging)

--- viewer/lighting.js
+++ viewer/lighting.js
@@ -4,44 +4,73 @@
 
 export default class Lighting {
 
-	constructor(viewer) {
+    constructor(viewer) {
 
-		this.viewer = viewer;
-		this.dir = [0.5, 0.5, 1.0];
-		this.color = [1.0, 1.0, 1.0];
-		this.ambientColor = [0.3, 0.3, 0.3];
-		this.intensity = 0.5;
-		this.buffer = new Float32Array(52);
+        this.viewer = viewer;
+        this._dir = [-0.5, -0.5, -1.0];
+        this._color = [1.0, 1.0, 1.0];
+        this._ambientColor = [0.3, 0.3, 0.3];
+        this._intensity = 0.5;
+        this._bufferData = new Float32Array(52);
+        this._buffer = null;
 
-		this.createLightingBuffer();
-	}
+        this._dirty = true;
+    }
 
-	createLightingBuffer() {
+    _update() {
+        if (!this._dirty) {
+            return;
 
-		var buffer = this.buffer;
+        }
+        var bufferData = this._bufferData;
 
-		buffer[0] = this.dir[0];
-		buffer[1] = this.dir[1];
-		buffer[2] = this.dir[2];
-		buffer[3] = 0; // unused
-		buffer[4] = this.color[0];
-		buffer[5] = this.color[1];
-		buffer[6] = this.color[2];
-		buffer[7] = 0; // unused
-		buffer[8] = this.ambientColor[0];
-		buffer[9] = this.ambientColor[1];
-		buffer[10] = this.ambientColor[2];
-		buffer[11] = 0; // unused;
-		buffer[12] = this.intensity;
+        bufferData[0] = this._dir[0];
+        bufferData[1] = this._dir[1];
+        bufferData[2] = this._dir[2];
+        bufferData[3] = 0; // unused
+        bufferData[4] = this._color[0];
+        bufferData[5] = this._color[1];
+        bufferData[6] = this._color[2];
+        bufferData[7] = 0; // unused
+        bufferData[8] = this._ambientColor[0];
+        bufferData[9] = this._ambientColor[1];
+        bufferData[10] = this._ambientColor[2];
+        bufferData[11] = 0; // unused;
+        bufferData[12] = this._intensity;
 
-		var gl = this.viewer.gl;
+        var gl = this.viewer.gl;
 
-		this.lightingBuffer = gl.createBuffer();
-		gl.bindBuffer(gl.UNIFORM_BUFFER, this.lightingBuffer);
-		gl.bufferData(gl.UNIFORM_BUFFER, buffer, gl.DYNAMIC_DRAW, 0, 52);
-	}
+        this._buffer = gl.createBuffer();
+        gl.bindBuffer(gl.UNIFORM_BUFFER, this._buffer);
+        gl.bufferData(gl.UNIFORM_BUFFER, bufferData, gl.DYNAMIC_DRAW, 0, 52);
 
-	draw() {
+        this._dirty = false;
+    }
 
-	}
+    setDir(dir) {
+        this.dir.set(dir);
+        this._dirty = true;
+    }
+
+    setColor(color) {
+        this._color.set(color);
+        this._dirty = true;
+    }
+
+    setAmbientColor(ambientColor) {
+        this._ambientColor.set(ambientColor);
+        this._dirty = true;
+    }
+
+    setIntensity(intensity) {
+        this.intensity = intensity;
+        this._dirty = true;
+    }
+
+    render(uniformBlockLocation) {
+        if (this._dirty) {
+            this._update();
+        }
+        this.viewer.gl.bindBufferBase(this.viewer.gl.UNIFORM_BUFFER, uniformBlockLocation, this._buffer);
+    }
 }
