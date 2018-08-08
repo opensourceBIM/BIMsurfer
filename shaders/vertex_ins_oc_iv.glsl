64cdd0f9797b097e9999f3405273963e20d79657
xeolabs <lindsay.kay@xeolabs.com>
2018-08-08 10:00:14 +0200

    Shading refinements; enable shading

--- shaders/vertex_ins_oc_iv.glsl
+++ shaders/vertex_ins_oc_iv.glsl
@@ -16,19 +16,18 @@ uniform mat4 viewNormalMatrix;
 uniform mat4 viewMatrix;
 
 uniform LightData {
-	vec3 lightPosition;
-	vec3 lightColor;
+	vec3 dir;
+	vec3 color;
 	vec3 ambientColor;
-	float shininess;
+	float intensity;
 } lightData;
 
 out mediump vec4 color;
 
 void main(void) {
   vec4 floatVertex = vertexQuantizationMatrix * vec4(float(vertexPosition.x), float(vertexPosition.y), float(vertexPosition.z), 1);
-  vec3 viewNormal = vec3( viewNormalMatrix * instanceNormalMatrices * vec4(vertexNormal, 0.0));
-  vec3 lightDir = vec3(0.5, 0.5, 0.5);
-  float lambertian = max(dot(viewNormal, lightDir), 0.0);
+  vec3 viewNormal = normalize(vec3( viewNormalMatrix * instanceNormalMatrices * vec4(vertexNormal, 0.0)));
+  float lambertian = max(dot(viewNormal, normalize(lightData.dir)), 0.0);
 
   gl_Position = projectionMatrix * viewMatrix * instanceMatrices * floatVertex;
   color = lambertian * objectColor;
