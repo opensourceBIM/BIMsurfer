277c89e71af9f8461be48f291acb450ba90685fc
Ruben de Laat <ruben@logic-labs.nl>
2018-08-09 09:45:49 +0200

    Fixed another copy/paste error

--- shaders/vertex_ins_iv_qc.glsl
+++ shaders/vertex_ins_iv_qc.glsl
@@ -4,7 +4,7 @@ precision mediump int;
 precision mediump float;
 
 in ivec3 vertexPosition;
-in ivec3 vertexNormal;
+in vec3 vertexNormal;
 in uvec4 vertexColor;
 
 in mat4 instanceMatrices;
@@ -28,9 +28,9 @@ void main(void) {
 
   vec4 floatVertex = vertexQuantizationMatrix * vec4(float(vertexPosition.x), float(vertexPosition.y), float(vertexPosition.z), 1);
   vec4 floatColor = vec4(float(vertexColor.x) / 255.0, float(vertexColor.y) / 255.0, float(vertexColor.z) / 255.0, float(vertexColor.w) / 255.0);
-  vec3 viewNormal = normalize(vec3( viewNormalMatrix * instanceNormalMatrices * vec4(float(vertexNormal.x) / 127.0, float(vertexNormal.y) / 127.0, float(vertexNormal.z) / 127.0, 0.0)));
+  vec3 viewNormal = vec3(normalize(viewNormalMatrix * instanceNormalMatrices * vec4(vertexNormal.xyz, 0.0)));
   float lambertian = max(dot(-viewNormal, normalize(lightData.dir)), 0.0);
 
   gl_Position = projectionMatrix * viewMatrix * instanceMatrices * floatVertex;
-  color = lambertian * floatColor;
+  color = vec4(lambertian * floatColor.rgb, floatColor.a);
 }
