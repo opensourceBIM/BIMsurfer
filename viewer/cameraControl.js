22628cabbde2a5ce0c5f651679e2a9e09bbb8049
Ruben de Laat <Ruben de Laat@LAPTOP-493FREB8.tsn.tno.nl>
2018-08-10 16:33:31 +0200

    Refactored so that all methods are members of the CameraControl class. Added cleanup method that cleans up the handles that cannot be auto-cleaned-up

--- viewer/cameraControl.js
+++ viewer/cameraControl.js
@@ -2,201 +2,223 @@ export default class CameraControl {
 
     constructor(viewer) {
 
-        var self = this;
-
         this.viewer = viewer;
 
         this.mousePanSensitivity = 0.5;
         this.mouseOrbitSensitivity = 0.5;
         this.canvasPickTolerance = 4;
 
-        var canvas = viewer.canvas;
-        var camera = viewer.camera;
+        this.canvas = viewer.canvas;
+        this.camera = viewer.camera;
 
-        var mousePos = vec2.create();
-        var mouseDownPos = vec2.create();
-        var over = false; // True when mouse over canvas
-        var down = false; // True when any mouse button is down
-        var lastX; // Last canvas pos while dragging
-        var lastY;
-        var mouseDownLeft; // Mouse button states
-        var mouseDownMiddle;
-        var mouseDownRight;
+        this.mousePos = vec2.create();
+        this.mouseDownPos = vec2.create();
+        this.over = false; // True when mouse over canvas
+        this.down = false; // True when any mouse button is down
+        this.lastX; // Last canvas pos while dragging
+        this.lastY;
+        this.mouseDownLeft; // Mouse button states
+        this.mouseDownMiddle;
+        this.mouseDownRight;
 
-        function getCanvasPosFromEvent(event, canvasPos) {
-            if (!event) {
-                event = window.event;
-                canvasPos[0] = event.x;
-                canvasPos[1] = event.y;
-            } else {
-                var element = event.target;
-                var totalOffsetLeft = 0;
-                var totalOffsetTop = 0;
-                while (element.offsetParent) {
-                    totalOffsetLeft += element.offsetLeft;
-                    totalOffsetTop += element.offsetTop;
-                    element = element.offsetParent;
-                }
-                canvasPos[0] = event.pageX - totalOffsetLeft;
-                canvasPos[1] = event.pageY - totalOffsetTop;
-            }
-            return canvasPos;
-        }
-
-        function getZoomRate() {
-            var modelBounds = viewer.modelBounds;
-            if (modelBounds) {
-                var xsize = modelBounds[3] - modelBounds[0];
-                var ysize = modelBounds[4] - modelBounds[1];
-                var zsize = modelBounds[5] - modelBounds[2];
-                var max = (xsize > ysize ? xsize : ysize);
-                max = (zsize > max ? zsize : max);
-                return max / 20;
-            } else {
-                return 1;
-            }
-        }
-
-        var getEyeLookDist = (function () {
-            var vec = vec3.create();
-            return function () {
-                return vec3.length(vec3.subtract(vec, viewer.camera.target, viewer.camera.eye));
-            };
-        })();
-
-        canvas.oncontextmenu = function (e) {
+        this.canvas.oncontextmenu = (e) => {
             e.preventDefault();
         };
 
-        canvas.addEventListener("mousedown", function (e) {
-            getCanvasPosFromEvent(e, mousePos);
-            switch (e.which) {
-                case 1:
-                    mouseDownLeft = true;
-                    lastX = mousePos[0];
-                    lastY = mousePos[1];
-                    mouseDownPos.set(mousePos);
-                    break;
-                case 2:
-                    mouseDownMiddle = true;
-                    break;
-                case 3:
-                    mouseDownRight = true;
-                    lastX = mousePos[0];
-                    lastY = mousePos[1];
-                    break;
-                default:
-                    break;
-            }
-            over = true;
-            down = true;
+        this.canvas.addEventListener("mousedown", (e) => {
+        	this.canvasMouseDown(e);
+        });
+
+        this.canvas.addEventListener("mouseup", (e) => {
+        	this.canvasMouseUp(e);
+        });
+
+        this.documentMouseUpHandler = (e) => {
+        	this.documentMouseUp(e);
+        };
+        document.addEventListener("mouseup", this.documentMouseUpHandler);
+
+        this.canvas.addEventListener("mouseenter", (e) => {
+            this.over = true;
             e.preventDefault();
         });
 
-        canvas.addEventListener("mouseup", function (e) {
-            getCanvasPosFromEvent(e, mousePos);
-            switch (e.which) {
-                case 1:
-                    mouseDownLeft = false;
-                    if (closeEnoughCanvas(mouseDownPos, mousePos)) {
-                        var viewObject = self.viewer.pick({canvasPos: mousePos});
-                        if (viewObject) {
-                            var aabb = viewObject.aabb;
-                            var center = [(aabb[0] + aabb[3]) / 2, (aabb[1] + aabb[4]) / 2, (aabb[2] + aabb[5]) / 2];
-                            self.viewer.camera.target = center;
-                            console.log("Picked: " + viewObject.type);
-
-                            console.log("Picked", viewObject);
-                        }
-                    }
-                    break;
-                case 2:
-                    mouseDownMiddle = false;
-                    break;
-                case 3:
-                    mouseDownRight = false;
-                    break;
-                default:
-                    break;
-            }
-            down = false;
+        this.canvas.addEventListener("mouseleave", (e) => {
+            this.over = false;
             e.preventDefault();
         });
 
-        document.addEventListener("mouseup", function (e) {
-            switch (e.which) {
-                case 1:
-                    mouseDownLeft = false;
-                    break;
-                case 2:
-                    mouseDownMiddle = false;
-                    break;
-                case 3:
-                    mouseDownRight = false;
-                    break;
-                default:
-                    break;
-            }
-            down = false;
+        this.canvas.addEventListener("mousemove", (e) => {
+        	this.canvasMouseMove(e);
         });
 
-        canvas.addEventListener("mouseenter", function (e) {
-            over = true;
-            e.preventDefault();
-        });
-
-        canvas.addEventListener("mouseleave", function (e) {
-            over = false;
-            e.preventDefault();
-        });
-
-        canvas.addEventListener("mousemove", function (e) {
-            if (!over) {
-                return;
-            }
-            if (down) {
-                getCanvasPosFromEvent(e, mousePos);
-                var x = mousePos[0];
-                var y = mousePos[1];
-                var xDelta = (x - lastX);
-                var yDelta = (y - lastY);
-                lastX = x;
-                lastY = y;
-                if (mouseDownLeft) { // Orbiting
-                    let f = 0.5;
-                    if (xDelta !== 0) {
-                        camera.orbitYaw(-xDelta * self.mouseOrbitSensitivity * f);
-                    }
-                    if (yDelta !== 0) {
-                        camera.orbitPitch(yDelta * self.mouseOrbitSensitivity * f);
-                    }
-                } else if (mouseDownRight) { // Panning
-                    var f = getEyeLookDist() / 600;
-                    camera.pan([xDelta * f, yDelta * self.mousePanSensitivity * f, 0.0]);
-                }
-            }
-            e.preventDefault();
-        });
-
-        canvas.addEventListener("wheel", function (e) { // Zooming
-            var delta = Math.max(-1, Math.min(1, -e.deltaY * 40));
-            if (delta === 0) {
-                return;
-            }
-            var d = delta / Math.abs(delta);
-            var zoom = -d * getZoomRate() * self.mousePanSensitivity;
-            camera.zoom(zoom);
-            e.preventDefault();
+        this.canvas.addEventListener("wheel", (e) => {
+        	this.canvasWheel(e);
         });
 
         // Returns true if the two Canvas-space points are
         // close enough to be considered the same point
 
-        function closeEnoughCanvas(p, q) {
-            return p[0] >= (q[0] - self.canvasPickTolerance) &&
-                p[0] <= (q[0] + self.canvasPickTolerance) &&
-                p[1] >= (q[1] - self.canvasPickTolerance) &&
-                p[1] <= (q[1] + self.canvasPickTolerance);
+    }
+    
+    getCanvasPosFromEvent(event, canvasPos) {
+        if (!event) {
+            event = window.event;
+            canvasPos[0] = event.x;
+            canvasPos[1] = event.y;
+        } else {
+            var element = event.target;
+            var totalOffsetLeft = 0;
+            var totalOffsetTop = 0;
+            while (element.offsetParent) {
+                totalOffsetLeft += element.offsetLeft;
+                totalOffsetTop += element.offsetTop;
+                element = element.offsetParent;
+            }
+            canvasPos[0] = event.pageX - totalOffsetLeft;
+            canvasPos[1] = event.pageY - totalOffsetTop;
         }
+        return canvasPos;
+    }
+
+    getZoomRate() {
+        var modelBounds = this.viewer.modelBounds;
+        if (modelBounds) {
+            var xsize = modelBounds[3] - modelBounds[0];
+            var ysize = modelBounds[4] - modelBounds[1];
+            var zsize = modelBounds[5] - modelBounds[2];
+            var max = (xsize > ysize ? xsize : ysize);
+            max = (zsize > max ? zsize : max);
+            return max / 20;
+        } else {
+            return 1;
+        }
+    }
+    
+    canvasMouseDown(e) {
+        this.getCanvasPosFromEvent(e, this.mousePos);
+        switch (e.which) {
+            case 1:
+                this.mouseDownLeft = true;
+                this.lastX = this.mousePos[0];
+                this.lastY = this.mousePos[1];
+                this.mouseDownPos.set(this.mousePos);
+                break;
+            case 2:
+            	this.mouseDownMiddle = true;
+                break;
+            case 3:
+            	this.mouseDownRight = true;
+            	this.lastX = this.mousePos[0];
+            	this.lastY = this.mousePos[1];
+                break;
+            default:
+                break;
+        }
+        this.over = true;
+        this.down = true;
+        e.preventDefault();
+    }
+    
+    canvasMouseUp(e) {
+        this.getCanvasPosFromEvent(e, this.mousePos);
+        switch (e.which) {
+            case 1:
+            	this.mouseDownLeft = false;
+                if (this.closeEnoughCanvas(this.mouseDownPos, this.mousePos)) {
+                    var viewObject = this.viewer.pick({canvasPos: this.mousePos});
+                    if (viewObject) {
+                        var aabb = viewObject.aabb;
+                        var center = [(aabb[0] + aabb[3]) / 2, (aabb[1] + aabb[4]) / 2, (aabb[2] + aabb[5]) / 2];
+                        this.viewer.camera.target = center;
+
+                        console.log("Picked", viewObject);
+                    }
+                }
+                break;
+            case 2:
+            	this.mouseDownMiddle = false;
+                break;
+            case 3:
+            	this.mouseDownRight = false;
+                break;
+            default:
+                break;
+        }
+        this.down = false;
+        e.preventDefault();
+    }
+    
+    canvasWheel(e) {
+        var delta = Math.max(-1, Math.min(1, -e.deltaY * 40));
+        if (delta === 0) {
+            return;
+        }
+        var d = delta / Math.abs(delta);
+        var zoom = -d * this.getZoomRate() * this.mousePanSensitivity;
+        this.camera.zoom(zoom);
+        e.preventDefault();
+    }
+
+    closeEnoughCanvas(p, q) {
+        return p[0] >= (q[0] - this.canvasPickTolerance) &&
+            p[0] <= (q[0] + this.canvasPickTolerance) &&
+            p[1] >= (q[1] - this.canvasPickTolerance) &&
+            p[1] <= (q[1] + this.canvasPickTolerance);
+    }
+    
+    canvasMouseMove(e) {
+        if (!this.over) {
+            return;
+        }
+        if (this.down) {
+        	this.getCanvasPosFromEvent(e, this.mousePos);
+            var x = this.mousePos[0];
+            var y = this.mousePos[1];
+            var xDelta = (x - this.lastX);
+            var yDelta = (y - this.lastY);
+            this.lastX = x;
+            this.lastY = y;
+            if (this.mouseDownLeft) { // Orbiting
+                let f = 0.5;
+                if (xDelta !== 0) {
+                	this.camera.orbitYaw(-xDelta * this.mouseOrbitSensitivity * f);
+                }
+                if (yDelta !== 0) {
+                	this.camera.orbitPitch(yDelta * this.mouseOrbitSensitivity * f);
+                }
+            } else if (this.mouseDownRight) { // Panning
+                var f = this.getEyeLookDist() / 600;
+                this.camera.pan([xDelta * f, yDelta * this.mousePanSensitivity * f, 0.0]);
+            }
+        }
+        e.preventDefault();
+    }
+    
+    documentMouseUp(e) {
+        switch (e.which) {
+	        case 1:
+	        	this.mouseDownLeft = false;
+	            break;
+	        case 2:
+	        	this.mouseDownMiddle = false;
+	            break;
+	        case 3:
+	        	this.mouseDownRight = false;
+	            break;
+	        default:
+	            break;
+	    }
+	    this.down = false;
+    }
+    
+    getEyeLookDist() {
+        var vec = vec3.create();
+        return vec3.length(vec3.subtract(vec, this.viewer.camera.target, this.viewer.camera.eye));
+    }
+
+    cleanup() {
+    	// TODO remove all event listeners, if we don't, a reference to the viewer and basically everything will stay in memory
+    	document.removeEventListener("mouseup", this.documentMouseUpHandler);
     }
 }
