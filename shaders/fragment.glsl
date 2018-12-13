41aa24501b3f1db0acadb8a46f605c92159bc6fd
Ruben de Laat <ruben@logic-labs.nl>
2018-12-13 10:11:15 +0100

    To lowp

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,14 +1,14 @@
-precision mediump int;
-precision mediump float;
+precision lowp int;
+precision lowp float;
 
 #ifdef WITH_PICKING
-flat in mediump uvec4 color;
-layout(location = 0) out mediump uvec4 myOutputColor;
+flat in uvec4 color;
+layout(location = 0) out uvec4 myOutputColor;
 
 in float depth;
 layout(location = 1) out float myOutputDepth;
 #else
-in mediump vec4 color;
+in vec4 color;
 out vec4 myOutputColor;
 #endif
 
