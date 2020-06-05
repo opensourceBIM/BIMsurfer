b457cc1febea2ea17ddf14ea1384c99aa966ce98
Ruben de Laat <ruben@logic-labs.nl>
2020-06-05 12:11:26 +0200

    Removed unnecessary matrix manipulation

--- viewer/camera.js
+++ viewer/camera.js
@@ -46,7 +46,7 @@ export class Camera {
         this._modelBounds = null;
 
         this.tempMat4 = mat4.create();
-        this.tempMat4b = mat4.create();
+        this.tempMat3b = mat3.create();
         this.tempVec3 = vec3.create();
         this.tempVec3b = vec3.create();
         this.tempVec3c = vec3.create();
@@ -131,11 +131,9 @@ export class Camera {
         }
 
         mat4.lookAt(this._viewMatrix, this._eye, this._target, this._up);
-        mat4.identity(this.tempMat4);
-        mat4.multiply(this._viewMatrix, this.tempMat4, this._viewMatrix); // Why?
-        mat3.fromMat4(this.tempMat4b, this._viewMatrix);
-        mat3.invert(this.tempMat4b, this.tempMat4b);
-        mat3.transpose(this._viewNormalMatrix, this.tempMat4b);
+        mat3.fromMat4(this.tempMat3b, this._viewMatrix);
+        mat3.invert(this.tempMat3b, this.tempMat3b);
+        mat3.transpose(this._viewNormalMatrix, this.tempMat3b);
         
         let [near, far] = [+Infinity, -Infinity];
 
