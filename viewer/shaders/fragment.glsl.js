39b60226aaa28ecb994403c9f410e4742d88e6d0
Ruben de Laat <ruben@bimbase.nl>
2020-11-24 21:26:08 +0100

    Temporary rollback of https://github.com/opensourceBIM/BIMsurfer/commit/497c369dca3e83bc3c0ac6b0a18746da99567480. For some models this solution did not work.

--- viewer/shaders/fragment.glsl.js
+++ viewer/shaders/fragment.glsl.js
@@ -8,8 +8,8 @@ in vec3 worldCoords;
 #ifdef WITH_PICKING
 flat in uvec4 color;
 layout(location = 0) out uvec4 myOutputColor;
-layout(location = 1) out uvec4 myOutputDepth;
-layout(location = 2) out uvec4 myOutputNormal;
+layout(location = 1) out float myOutputDepth;
+layout(location = 2) out vec4 myOutputNormal;
 #else
 in vec4 color;
 layout(location = 0) out vec4 myOutputColor;
@@ -32,13 +32,21 @@ void main(void) {
 
 #ifdef WITH_PICKING
    myOutputColor = color;
+   myOutputDepth = gl_FragCoord.z;
+  // The picking program does not have normal attributes, so we *have* to
+   // use the shader derivatives. @todo reevaluate
+   myOutputNormal.xyz = normalize(cross(dFdx(worldCoords), dFdy(worldCoords)));
+
+  // The next bit is disabled for now because precision was not ok
    // TODO probably should check whether 24 bits of precision are available
-   uint intDepth = uint(gl_FragCoord.z * float(uint(1) << uint(24)));
-   myOutputDepth = uvec4(intDepth >> 24, intDepth >> 16, intDepth >> 8, 0);
+//   uint intDepth = uint(gl_FragCoord.z * float(uint(1) << uint(24)));
+//   myOutputDepth = uvec4(intDepth >> 24, intDepth >> 16, intDepth >> 8, 0);
    // The picking program does not have normal attributes, so we *have* to
    // use the shader derivatives. @todo reevaluate
-   vec3 normalizedNormal = normalize(cross(dFdx(worldCoords), dFdy(worldCoords))); 
-   myOutputNormal = uvec4(int(normalizedNormal.x * 127.0), int(normalizedNormal.y * 127.0), int(normalizedNormal.z * 127.0), 0);
+//   vec3 normalizedNormal = normalize(cross(dFdx(worldCoords), dFdy(worldCoords))); 
+//   myOutputNormal = uvec4(int(normalizedNormal.x * 127.0), int(normalizedNormal.y * 127.0), int(normalizedNormal.z * 127.0), 0);
+
+
 #else
   // TODO if we move the lighting to the fragment shader, we can enable back-face-culling (for those objects that can handle it) and use gl_FrontFacing to decide to invert the normal)
    myOutputColor = color;// vec4(color.rgb * color.a, color.a);
